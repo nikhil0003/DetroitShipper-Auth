4ac41945d636e1935b9a6b6a81ba7e50584cb751
nikhil Kumar <mail2nikhilkumar3@gmail.com>
2023-01-05 17:19:34 +0530

    first commit2

---
+++ src/test/java/com/detroitauctionshippers/DetroitShipperAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.detroitauctionshippers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DetroitShipperAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
