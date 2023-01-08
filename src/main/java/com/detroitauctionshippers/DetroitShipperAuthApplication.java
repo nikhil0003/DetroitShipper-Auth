3a26930f39b9fffa2fc9cf278c828031ac84596a
mail2 <mail2nikhilkumar3@gmail.com>
2023-01-08 20:21:38 +0530

    Correction

---
+++ src/main/java/com/detroitauctionshippers/DetroitShipperAuthApplication.java
@@ -0,0 +1,13 @@
+package com.detroitauctionshippers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DetroitShipperAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DetroitShipperAuthApplication.class, args);
+	}
+
+}
